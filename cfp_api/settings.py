3e271551419d1982ba52f150ff51feae96121595
Williamdreams89 <wdanquah@stu.ucc.edu.gh>
2023-01-22 17:15:11 +0000

    Requirements file and serializer init

--- cfp_api/settings.py
+++ cfp_api/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    'accounts_api'
+    'accounts_api', 
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
