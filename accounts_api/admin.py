6d2271a8dbffe2c043d51431eb392842376520de
Williamdreams89 <wdanquah@stu.ucc.edu.gh>
2023-01-22 17:51:39 +0000

    Admin dashboard

--- accounts_api/admin.py
+++ accounts_api/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import User 
 
-# Register your models here.
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['id', 'first_name', 'last_name', 'email', 'date_added']
+admin.site.register(User, UserAdmin)
