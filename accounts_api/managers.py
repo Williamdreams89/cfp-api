28e1cfe3c2e5f8434ae971cb7d9f57c8789e07a2
Williamdreams89 <wdanquah@stu.ucc.edu.gh>
2023-01-22 18:49:15 +0000

    User manager Conflict

--- accounts_api/managers.py
+++ accounts_api/managers.py
@@ -3,10 +3,10 @@ from django.contrib.auth.models import BaseUserManager
 class UserManager(BaseUserManager):
     """Overriding the default User Manager"""
 
-    def create_user(self, email, first_name, last_name, password=None):
+    def create_user(self, email, first_name, last_name, user_type,password=None):
         if not email:
             raise ValueError("Invalid field, email must be provided !!")
-        user = self.model(email=self.normalize_email(email), first_name=first_name, last_name=last_name)
+        user = self.model(email=self.normalize_email(email), first_name=first_name, last_name=last_name, user_type=user_type)
         user.set_password(password)
         user.save()
         return user 
@@ -18,4 +18,3 @@ class UserManager(BaseUserManager):
         user.save()
         return user 
 
-        
