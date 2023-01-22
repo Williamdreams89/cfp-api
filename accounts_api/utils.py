452861543105170ae538d0ec96daedd5c4a6fc10
Williamdreams89 <wdanquah@stu.ucc.edu.gh>
2023-01-22 23:01:10 +0000

    Emailing

--- accounts_api/utils.py
+++ accounts_api/utils.py
@@ -3,5 +3,5 @@ from django.core.mail import EmailMessage
 class Util:
     @staticmethod
     def send_email(data):
-        # email = EmailMessage()
-        pass
+        email = EmailMessage(subject=data["email_subject"],body=data["email_body"], from_email="support@livingcareservices.org", to=(data["email_to"],))
+        email.send()
