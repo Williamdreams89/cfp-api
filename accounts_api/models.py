902afc26c84bf647dc7d67cdb7c4429d81f2c53d
Williamdreams89 <wdanquah@stu.ucc.edu.gh>
2023-01-22 17:11:14 +0000

    Customed User model

--- accounts_api/models.py
+++ accounts_api/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from .managers import UserManager
 
-# Create your models here.
+class User(AbstractBaseUser, PermissionsMixin):
+    class UserChoice(models.TextChoices):
+        COMM_PERSONNEL = ("COMM_PERSONNEL", "Community Personnel",)
+        ADMIN = ("ADMIN", "Admin",)
+        PUBLIC_OFFICER = ("PUBLIC OFFICER", "Public Officer",)
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    user = models.CharField(max_length=100, choices=UserChoice.choices, default=UserChoice.ADMIN)
+    is_active = models.BooleanField(default=False)
+    is_verified = models.BooleanField(default=False)
+    date_added = models.DateField(auto_created=True)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+
+    class Meta:
+        ordering = ["-date_added"]
