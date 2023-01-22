63bbad7089028bccc37b7d00193e3dcc58595b53
Williamdreams89 <wdanquah@stu.ucc.edu.gh>
2023-01-22 18:01:03 +0000

    Fixed admin login bug

--- accounts_api/models.py
+++ accounts_api/models.py
@@ -11,7 +11,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     user = models.CharField(max_length=100, choices=UserChoice.choices, default=UserChoice.ADMIN)
-    is_active = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=True)
     is_verified = models.BooleanField(default=False)
     date_added = models.DateField(auto_now_add=True)
 
