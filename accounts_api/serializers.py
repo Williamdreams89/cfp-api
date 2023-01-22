0a02032b240203c70cdf05fc643801d056f80b67
Williamdreams89 <wdanquah@stu.ucc.edu.gh>
2023-01-22 18:02:39 +0000

    User registratio bug

--- accounts_api/serializers.py
+++ accounts_api/serializers.py
@@ -9,3 +9,7 @@ class UserSignUpSerializer(serializers.ModelSerializer):
         extra_kwargs = {
             "password":{"write_only": True}
         }
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user 
